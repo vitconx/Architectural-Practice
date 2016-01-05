498e6af7b667fcd0813b8cc70e116f50681601bb
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2016-01-05 16:37:41 +0700

    add Data Mapper.

--- data/src/main/java/com/pycogroup/data/entity/mapper/UserEntityDataMapper.java
+++ data/src/main/java/com/pycogroup/data/entity/mapper/UserEntityDataMapper.java
@@ -3,6 +3,9 @@ package com.pycogroup.data.entity.mapper;
 import com.pycogroup.data.entity.UserEntity;
 import com.pycogroup.domain.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -17,6 +20,27 @@ public class UserEntityDataMapper {
     }
 
     public User transform(UserEntity userEntity) {
-        return null;
+        User user = null;
+        if (userEntity != null) {
+            user = new User(userEntity.getUserId());
+            user.setCoverUrl(userEntity.getCoverUrl());
+            user.setFullName(userEntity.getFullname());
+            user.setDescription(userEntity.getDescription());
+            user.setFollowers(userEntity.getFollowers());
+            user.setEmail(userEntity.getEmail());
+        }
+        return user;
+    }
+
+    public List<User> transform(List<UserEntity> userEntityList) {
+        List<User> userList = new ArrayList<>();
+        User user;
+        for (UserEntity userEntity : userEntityList) {
+            user = transform(userEntity);
+            if (user != null) {
+                userList.add(user);
+            }
+        }
+        return userList;
     }
 }
--- data/src/test/java/com/pycogroup/data/entity/mapper/UserEntityDataMapperTest.java
+++ data/src/test/java/com/pycogroup/data/entity/mapper/UserEntityDataMapperTest.java
@@ -1,16 +1,62 @@
 package com.pycogroup.data.entity.mapper;
 
+import com.pycogroup.data.ApplicationTestCase;
+import com.pycogroup.data.entity.UserEntity;
+import com.pycogroup.domain.User;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by HieuVP on 1/5/16.
  */
-public class UserEntityDataMapperTest {
+public class UserEntityDataMapperTest extends ApplicationTestCase {
+    private static final int FAKE_USER_ID = 123;
+    private static final String FAKE_FULLNAME = "Tony Stark";
+
+    private UserEntityDataMapper userEntityDataMapper;
+
+    @Before
+    public void setUp() throws Exception {
+        userEntityDataMapper = new UserEntityDataMapper();
+    }
 
     @Test
-    public void testTransform() throws Exception {
+    public void testTransformUserEntity() {
+        UserEntity userEntity = createFakeUserEntity();
+        User user = userEntityDataMapper.transform(userEntity);
+        assertThat(user, is(instanceOf(User.class)));
+        assertThat(user.getUserId(), is(FAKE_USER_ID));
+        assertThat(user.getFullName(), is(FAKE_FULLNAME));
+    }
 
+    @Test
+    public void testTransformUserEntityList() {
+        UserEntity mockUserEntityOne = mock(UserEntity.class);
+        UserEntity mockUserEntityTwo = mock(UserEntity.class);
+
+        List<UserEntity> userEntityList = new ArrayList<>();
+        userEntityList.add(mockUserEntityOne);
+        userEntityList.add(mockUserEntityTwo);
+
+        List<User> userList = userEntityDataMapper.transform(userEntityList);
+        assertThat(userList.toArray()[0], is(instanceOf(User.class)));
+        assertThat(userList.toArray()[1], is(instanceOf(User.class)));
+        assertThat(userList.size(), is(2));
+    }
+
+    private UserEntity createFakeUserEntity() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(FAKE_USER_ID);
+        userEntity.setFullname(FAKE_FULLNAME);
+        return userEntity;
     }
 }
